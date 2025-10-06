10641357b1626896f50933de05ed699d27d3bf62
rehyan_yadav <ritulyadav1984@gmail.com>
2025-10-07 00:57:26 +0530

    first commit

---
+++ basics/helloworld.go
@@ -0,0 +1,19 @@
+// package main // package declaration
+// import ("fmt") // import statement
+// func main(){ // main function
+// 	fmt.Println(" hello world ")
+
+// }
+
+/*
+To run this Go program, open your terminal and navigate to the directory containing `helloworld.go`. Then execute:
+
+go run helloworld.go
+
+This will compile and run the program, printing " hello world " to the terminal.
+*/
+
+
+//compact vesrion is 
+
+package main; import ("fmt") ; func main(){ fmt.Println("hello world ")};
---
+++ comments.go
@@ -0,0 +1,14 @@
+// This is a comment
+package main
+import ("fmt")
+
+func main() {
+  // This is a comment
+  fmt.Println("Hello World!")
+}
+
+
+/*
+ this is the multiline comment
+ 
+*/

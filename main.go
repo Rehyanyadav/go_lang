c045c880b79b5f218980f3709c0c12ff931ae56c
rehyan_yadav <ritulyadav1984@gmail.com>
2025-10-11 22:13:57 +0530

    new topics

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"mylearning/myutil"
+)
+
+func main() {
+	fmt.Println("learning go lang by hello world")
+	myutil.PrintMessage("hello world")
+}
---
+++ print/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 25
+	name := "rehyan"
+	height := 5.2432323
+
+	fmt.Println("age: ", age, "height:", height, "name:", name)
+	fmt.Println("hello world")
+	fmt.Printf("age is %d \n ",age)
+
+}
